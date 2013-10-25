86a2961b84b15968dfb2deded2bf23f929aadd31
monsou <monsou@monsoumatoMacBook-Pro-2.local>
2013-10-25 15:23:51 +0800

    Initial Commit

---
+++ WSQ/AppDelegate.h
@@ -0,0 +1,19 @@
+//
+//  AppDelegate.h
+//  WSQ
+//
+//  Created by monsou on 13-10-25.
+//  Copyright (c) 2013年 monsou. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@class ViewController;
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+@property (strong, nonatomic) ViewController *viewController;
+
+@end
---
+++ WSQ/ViewController.h
@@ -0,0 +1,13 @@
+//
+//  ViewController.h
+//  WSQ
+//
+//  Created by monsou on 13-10-25.
+//  Copyright (c) 2013年 monsou. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+@end
